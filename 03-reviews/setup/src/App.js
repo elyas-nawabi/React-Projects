3206c89adb2a0488009314a4611516678a602d2b
bilal201419 <elyas.nawabi19@gmail.com>
2021-07-31 16:25:34 +0430

    third project finished

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -6,9 +6,9 @@ function App() {
       <div className='title'>
         <h2>our reviews</h2>
         <div className='underline'>
-
         </div>
       </div>
+      <Review />
     </section>
   </main>;
 }

77729295e61fbeeb536e737c4f082cebaa233278
bilal201419 <elyas.nawabi19@gmail.com>
2021-08-01 07:57:15 +0430

    first step title fetching

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+// import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+const Question = ({title, info}) => {
+  // const [mydata, setmydata] = useState(data);
+  const [infos, setInfos] = useState(false);
+ 
+  return <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={()=>setInfos(!infos)}>{infos?'close':'open'}</button>
+      </header>
+      <p>{infos?{info}:''}</p>
+    </article>
 };
 
 export default Question;
